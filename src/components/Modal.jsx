3dde6e640ae3bc9f4a7ff009aa8d9c1c2595ba74
wura-ola <wuraademola@gmail.com>
2024-02-18 22:23:31 +0100

    committed

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,13 +8,13 @@ function Modal({ isModalOpen, setIsModalOpen, children }) {
     <div className="flex justify-center items-center  ">
       {isModalOpen ? (
         <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm max-w-96 flex flex-col justify-center items-center">
-          <section className=" flex flex-col justify-center items-center m-auto  bg-white w-96 p-9 ">
+          <section className="rounded-lg flex flex-col justify-center  bg-white w-80 p-5 ">
             <h3 className="cursor-pointer">Delete Comment</h3>
             <p className="text-grayishBlue  text-start mt-3">
               Are you sure you want to delete this comment?This will remove the
               comment and it can't be undone
             </p>
-            <div className="flex justify-center mt-3 gap-7 mb-3">
+            <div className="flex  mt-3 gap-7 mb-3">
               <button
                 className="bg-gray-500 px-3 py-1  text-white rounded"
                 onClick={closeModal}
